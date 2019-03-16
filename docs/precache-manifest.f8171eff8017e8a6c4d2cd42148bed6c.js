3e0ec310549afbe73e89c7f0a51fc1d5ef3e0faa
hiredgun <turczyns@gmail.com>
2019-03-16 18:34:40 +0100

    build the app

---
+++ docs/precache-manifest.f8171eff8017e8a6c4d2cd42148bed6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7acf24d2d6a497d9f5fd",
+    "url": "/static/js/main.1096e66d.chunk.js"
+  },
+  {
+    "revision": "d4a11412c8932457d724",
+    "url": "/static/js/2.316988c9.chunk.js"
+  },
+  {
+    "revision": "7acf24d2d6a497d9f5fd",
+    "url": "/static/css/main.eca4a468.chunk.css"
+  },
+  {
+    "revision": "20d2ea5ec2b66f84cc19adbac2a83a64",
+    "url": "/index.html"
+  }
+];
