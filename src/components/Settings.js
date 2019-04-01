aa6fa68d2d082fbbe4be1c4949ad89b17450aa11
miguelagol <michalina.gol@gmail.com>
2019-04-01 22:30:11 +0200

    Refactor Settings component to React hooks

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,80 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { GoSettings } from 'react-icons/go';
 import Button from './Button';
 import css from './Settings.module.css';
 
-class Settings extends React.Component {
-    state = {
-        userCount: this.props.userCount,
-        input: '10',
-    };
+function Settings(props) {
+    const [userCount, setUserCount] = useState(props.userCount);
+    const [time, setTime] = useState('10');
 
-    handleSubmit = event => {
-        const { input, userCount } = this.state;
-        event.preventDefault();
-        this.props.onSubmit(input, userCount);
-    };
-
-    handleUserCountChange = event => {
-        this.setState({
-            userCount: event.target.value,
-        });
-    };
-
-    handleTimeChange = event => {
-        this.setState({
-            input: event.target.value,
-        });
-    };
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div className={css.settings}>
-                    <header className={css.header}>
-                        <h1>Settings</h1>
-                        <GoSettings className={css.icon} size={40} />
-                    </header>
-                    <main>
-                        <label>
-                            MAX TIME
-                            <input className={css['max-time-input']} onChange={this.handleTimeChange} />
-                        </label>
-                        <div className="users">
-                            USERS COUNT
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="users_count"
-                                    value="two"
-                                    checked={this.state.userCount === 'two'}
-                                    onChange={this.handleUserCountChange}
-                                />
-                                2
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="users_count"
-                                    value="four"
-                                    checked={this.state.userCount === 'four'}
-                                    onChange={this.handleUserCountChange}
-                                />
-                                4
-                            </label>
-                        </div>
-                    </main>
-                    <footer className={css.footer}>
-                        <Button type="button" onClick={this.props.onCancel} className="foo">
-                            CANCEL
-                        </Button>
-                        <Button variant="cta">SAVE</Button>
-                    </footer>
-                </div>
-            </form>
-        );
+    function handleUserCountChange(event) {
+        setUserCount(event.target.value);
     }
+
+    function handleTimeChange(event) {
+        setTime(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        props.onSubmit(time, userCount);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={css.settings}>
+                <header className={css.header}>
+                    <h1>Settings</h1>
+                    <GoSettings className={css.icon} size={40} />
+                </header>
+                <main>
+                    <label>
+                        MAX TIME
+                        <input className={css['max-time-input']} onChange={handleTimeChange} />
+                    </label>
+                    <div className="users">
+                        USERS COUNT
+                        <label>
+                            <input
+                                type="radio"
+                                name="users_count"
+                                value="two"
+                                checked={userCount === 'two'}
+                                onChange={handleUserCountChange}
+                            />
+                            2
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="users_count"
+                                value="four"
+                                checked={userCount === 'four'}
+                                onChange={handleUserCountChange}
+                            />
+                            4
+                        </label>
+                    </div>
+                </main>
+                <footer className={css.footer}>
+                    <Button type="button" onClick={props.onCancel} className="foo">
+                        CANCEL
+                    </Button>
+                    <Button variant="cta">SAVE</Button>
+                </footer>
+            </div>
+        </form>
+    );
 }
 
 Settings.propTypes = {
